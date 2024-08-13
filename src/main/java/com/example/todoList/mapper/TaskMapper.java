f25819acd40b27c4647a20dfb7dd585d2de5c8ea
Guilherme Dias <guidias200220@gmail.com>
2024-08-13 11:52:37 -0300

    feat: create TaskMapper

--- src/main/java/com/example/todoList/mapper/TaskMapper.java
+++ src/main/java/com/example/todoList/mapper/TaskMapper.java
@@ -1,2 +1,11 @@
-package com.example.todoList.mapper;public class TaskMapper {
+package com.example.todoList.mapper;
+
+
+import com.example.todoList.domain.Task;
+import com.example.todoList.dto.ResponseTaskDto;
+
+public class TaskMapper {
+    public static ResponseTaskDto toDto(Task task){
+        return new ResponseTaskDto(task.getId(), task.getName(), task.getDescription(), task.getAccomplished(), task.getPriority());
+    }
 }
